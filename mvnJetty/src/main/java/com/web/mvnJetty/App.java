e7bd53c7eec7ddd70de1e526c6951eba187a9be7
USER <USER@hb6003>
2016-08-25 13:13:24 +0900

    mvnJetty

---
+++ mvnJetty/src/main/java/com/web/mvnJetty/App.java
@@ -0,0 +1,13 @@
+package com.web.mvnJetty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
